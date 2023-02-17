f3b823f28a204a567c5abf291b0e4ae233a10eb0
Renzo Posobon <renzoposobon95@gmail.com>
2023-02-17 18:00:04 -0300

    ejercicio 2

---
+++ index.js
@@ -0,0 +1,12 @@
+const miPersona = {
+    nombre: "Renzo Posobon",
+    edad: 27,
+    desarrollador: true,
+    fechaDeNacimiento: 13/12/1995,
+    libroFavorito: {
+        "titulo": "El resplandor",
+        "autor": "Stephen King",
+        fecha: 1985,
+        url: undefined
+    }
+}
